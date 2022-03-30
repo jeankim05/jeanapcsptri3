cd76843c54302f6274b136514ef49cf7a8c380d5
Isaac Le <60956151+Isaac-Le@users.noreply.github.com>
2022-03-30 21:14:12 +0000

    Range of Factors

---
+++ crossover/prime.py
@@ -0,0 +1,16 @@
+def findprimes(min, max):
+    for Number in range(min, max + 1):
+        count = 0
+        for i in range(2, (Number // 2 + 1)):
+            if (Number % i == 0):
+                count = count + 1
+                break
+        if (count == 0 and Number != 1):
+            print(" %d" % Number, end='  ')
+    print()
+
+
+def menu():
+    minimum = int(input(" Enter the Minimum Value for Primes: "))
+    maximum = int(input(" Enter the Maximum Value for Primes: "))
+    findprimes(minimum, maximum)
